5f8be78945152ae62a3453f1a3f4ff6dcafbfe5d
ygpark <ygpark@hn-tech.co.kr>
2023-03-15 21:44:23 +0900

    -- Monitoring FMS Rest(EQP, TRAY)

---
+++ MonitorFMS/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace MonitorFMS
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dgTray = new System.Windows.Forms.DataGridView();
+            this.TColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTrayId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgEqp = new System.Windows.Forms.DataGridView();
+            this.Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtEqpId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTray)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEqp)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 459);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dgTray);
+            this.panel4.Controls.Add(this.txtTrayId);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(330, 33);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(325, 426);
+            this.panel4.TabIndex = 5;
+            // 
+            // dgTray
+            // 
+            this.dgTray.AllowUserToAddRows = false;
+            this.dgTray.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgTray.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTray.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TColumn,
+            this.TValue});
+            this.dgTray.Location = new System.Drawing.Point(6, 31);
+            this.dgTray.Name = "dgTray";
+            this.dgTray.RowHeadersVisible = false;
+            this.dgTray.RowTemplate.Height = 23;
+            this.dgTray.Size = new System.Drawing.Size(307, 383);
+            this.dgTray.TabIndex = 4;
+            // 
+            // TColumn
+            // 
+            this.TColumn.HeaderText = "Column";
+            this.TColumn.Name = "TColumn";
+            // 
+            // TValue
+            // 
+            this.TValue.HeaderText = "Value";
+            this.TValue.Name = "TValue";
+            // 
+            // txtTrayId
+            // 
+            this.txtTrayId.Location = new System.Drawing.Point(148, 4);
+            this.txtTrayId.Name = "txtTrayId";
+            this.txtTrayId.Size = new System.Drawing.Size(165, 21);
+            this.txtTrayId.TabIndex = 3;
+            this.txtTrayId.Text = "FIP-22-0014";
+            this.txtTrayId.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tray ID : ";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgEqp);
+            this.panel3.Controls.Add(this.txtEqpId);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(325, 426);
+            this.panel3.TabIndex = 4;
+            // 
+            // dgEqp
+            // 
+            this.dgEqp.AllowUserToAddRows = false;
+            this.dgEqp.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgEqp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEqp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column,
+            this.Value});
+            this.dgEqp.Location = new System.Drawing.Point(8, 31);
+            this.dgEqp.Name = "dgEqp";
+            this.dgEqp.RowHeadersVisible = false;
+            this.dgEqp.RowTemplate.Height = 23;
+            this.dgEqp.Size = new System.Drawing.Size(304, 383);
+            this.dgEqp.TabIndex = 3;
+            // 
+            // Column
+            // 
+            this.Column.HeaderText = "Column";
+            this.Column.Name = "Column";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // txtEqpId
+            // 
+            this.txtEqpId.Location = new System.Drawing.Point(156, 4);
+            this.txtEqpId.Name = "txtEqpId";
+            this.txtEqpId.Size = new System.Drawing.Size(156, 21);
+            this.txtEqpId.TabIndex = 2;
+            this.txtEqpId.Text = "F01FOR01100101";
+            this.txtEqpId.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Equipment ID :";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtURL);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtSec);
+            this.panel2.Controls.Add(this.txtCount);
+            this.panel2.Controls.Add(this.btnStop);
+            this.panel2.Controls.Add(this.btnSearch);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(655, 33);
+            this.panel2.TabIndex = 3;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(13, 3);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(299, 21);
+            this.txtURL.TabIndex = 7;
+            this.txtURL.Text = "http://localhost:48091";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "sec";
+            // 
+            // txtSec
+            // 
+            this.txtSec.Location = new System.Drawing.Point(322, 4);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(40, 21);
+            this.txtSec.TabIndex = 5;
+            this.txtSec.Text = "5";
+            // 
+            // txtCount
+            // 
+            this.txtCount.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtCount.Location = new System.Drawing.Point(423, 4);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(47, 21);
+            this.txtCount.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(568, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(478, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Start";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 459);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainFrm";
+            this.Text = "Monitor FMS Rest";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTray)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEqp)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtTrayId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEqpId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgEqp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridView dgTray;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TValue;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.TextBox txtURL;
+    }
+}
+
---
+++ MonitorFMS/Form1.cs
@@ -0,0 +1,252 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Security.Principal;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace MonitorFMS
+{
+    public partial class MainFrm : Form
+    {
+        private string _RestUri = string.Empty;
+        private string _APIKey = string.Empty;
+
+        private string _EQP_ID = string.Empty;
+        private string _TRAY_ID = string.Empty;
+
+        private bool _isRunning = false;
+        private bool _IsConnection = false;
+
+        private int _timerInterval = 10;
+        private int _currentCount = 0;
+
+        private Timer _Timer;
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            _RestUri = "http://localhost:48091";
+            _APIKey = "MPwjAXk0KkGPNLOykR5ZGQ==";
+
+            _timerInterval = txtSec.Text.Length > 0 ? Convert.ToInt32(txtSec.Text) : 10;
+
+            _Timer = new Timer();
+            _Timer.Interval = 1000;
+            _Timer.Tick += _Timer_Tick;
+
+        }
+
+        private void _Timer_Tick(object sender, EventArgs e)
+        {
+            if (getCheckRest())
+            {
+                if(_currentCount % _timerInterval == 0)
+                {
+                    _currentCount = _timerInterval;
+                    getStatus(_EQP_ID, _TRAY_ID);
+                }
+                txtCount.Text = _currentCount.ToString();
+                _currentCount--;
+                
+            }
+            else
+            {
+                changeTimer(false);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            _RestUri = txtURL.Text.Length > 0 ? txtURL.Text : "http://localhost:48091";
+
+            _timerInterval = txtSec.Text.Length > 0 ? Convert.ToInt32(txtSec.Text) : 10;
+            _currentCount = _timerInterval;
+
+            changeTimer(true);
+            //string eqp_result = await getEquipmentStatus(eqp_id);
+            //getStatus(eqp_id, tray_id);
+        }
+
+        private bool getCheckRest()
+        {
+            _EQP_ID = txtEqpId.Text ?? "";
+            _TRAY_ID = txtTrayId.Text ?? "";
+
+            if (string.IsNullOrEmpty(_EQP_ID))
+            {
+                MessageBox.Show("Enter the Equipment ID "); return false;
+            }
+
+            if (string.IsNullOrEmpty(_TRAY_ID))
+            {
+                MessageBox.Show("Enter the Tray ID "); return false;
+            }
+
+            return true;
+        }
+
+        private void changeTimer(bool isStart)
+        {
+            if(isStart)
+            {
+                _Timer.Start();
+                _isRunning = true;
+                btnSearch.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            else
+            {
+                _Timer.Stop();
+                _isRunning = false;
+                btnSearch.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+
+
+        private async void getStatus(string eqp_id, string tray_id)
+        {
+            txtCount.BackColor = System.Drawing.SystemColors.MenuHighlight;
+
+            var options = new RestClientOptions(_RestUri)
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest($"/fmsbiz/eqp/{eqp_id}", Method.Get);
+            request.AddHeader("APIKey", _APIKey);
+            RestResponse res_eqp = await client.ExecuteAsync(request);
+
+            string result_eqp = res_eqp.Content;
+
+            viewEquipment(result_eqp);
+
+            request = new RestRequest($"/fmsbiz/tray/{tray_id}", Method.Get);
+            request.AddHeader("APIKey", _APIKey);
+
+            RestResponse res_tray = await client.ExecuteAsync(request);
+
+            string result_tray = res_tray.Content;
+
+            viewTray(result_tray);
+
+            txtCount.BackColor = System.Drawing.SystemColors.ButtonFace;
+        }
+
+        private bool viewEquipment(string EQUIPMENT)
+        {
+            RESLUT_EQP json = JsonConvert.DeserializeObject<RESLUT_EQP>(EQUIPMENT);
+
+            dgEqp.Rows.Clear();
+
+            dgEqp.Rows.Add("eqp_type", json.data.eqp_type);
+            dgEqp.Rows.Add("eqp_id", json.data.eqp_id);
+            dgEqp.Rows.Add("tray_id", json.data.tray_id);
+            dgEqp.Rows.Add("fms_use", json.data.fms_use);
+            dgEqp.Rows.Add("op_mode", json.data.op_mode);
+            dgEqp.Rows.Add("op_proc", json.data.op_proc);
+            dgEqp.Rows.Add("op_maint", json.data.op_maint);
+            dgEqp.Rows.Add("job_start_time", json.data.job_start_time);
+            dgEqp.Rows.Add("job_end_time", json.data.job_end_time);
+            dgEqp.Rows.Add("fire_flag", json.data.fire_flag);
+
+            return true;
+        }
+
+        private bool viewTray(string TRAY)
+        {
+            RESLUT_TRAY json = JsonConvert.DeserializeObject<RESLUT_TRAY>(TRAY);
+
+            dgTray.Rows.Clear();
+
+            dgTray.Rows.Add("tray_id", json.data.tray_id);
+            dgTray.Rows.Add("model_id", json.data.model_id);
+            dgTray.Rows.Add("route_id", json.data.route_id);
+            dgTray.Rows.Add("lot_id", json.data.lot_id);
+            dgTray.Rows.Add("work_order_id", json.data.work_order_id);
+            dgTray.Rows.Add("full_flag", json.data.full_flag);
+            dgTray.Rows.Add("eqp_type", json.data.eqp_type);
+            dgTray.Rows.Add("oper_type", json.data.oper_type);
+            dgTray.Rows.Add("oper_no", json.data.oper_no);
+            dgTray.Rows.Add("next_eqp_type", json.data.next_eqp_type);
+            dgTray.Rows.Add("next_oper_type", json.data.next_oper_type);
+            dgTray.Rows.Add("job_start_time", json.data.job_start_time);
+            dgTray.Rows.Add("job_end_time", json.data.job_end_time);
+            dgTray.Rows.Add("oper_in_cells_csv", json.data.oper_in_cells_csv);
+            dgTray.Rows.Add("current_cells_csv", json.data.current_cells_csv);
+
+            return true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            changeTimer(false);
+        }
+    }
+
+    public class RESLUT_EQP
+    {
+        public int count { get; set; }
+        public string table { get; set; }
+        public TB_EQP data { get; set; }
+    }
+
+    public class TB_EQP
+    {
+        public string eqp_type { get; set; }
+        public string eqp_id { get; set; }
+        public string tray_id { get; set; }
+        public string fms_use { get; set; }
+        public string op_mode { get; set; }
+        public string op_proc { get; set; }
+        public string op_maint { get; set; }
+        public string job_start_time { get; set; }
+        public string job_end_time { get; set; }
+        public string fire_flag { get; set; }
+    }
+
+    public class RESLUT_TRAY
+    {
+        public int count { get; set; }
+        public string table { get; set; }
+        public TB_TRAY data { get; set; }
+    }
+
+    public class TB_TRAY 
+    {
+        public string tray_id { get; set; }
+        public string model_id { get; set; }
+        public string route_id { get; set; }
+        public string lot_id { get; set; }
+        public string work_order_id { get; set; }
+        public string full_flag { get; set; }
+        public string eqp_type { get; set; }
+        public string oper_type { get; set; }
+        public string oper_no { get; set; }
+
+        public string next_eqp_type { get; set; }
+
+        public string next_oper_type { get; set; }
+
+        public string job_start_time { get; set; }
+        public string job_end_time { get; set; }
+
+        public string oper_in_cells_csv { get; set; }
+
+        public string current_cells_csv { get; set; }
+    }
+
+
+}
